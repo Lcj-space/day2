5de4d8aa90543d5472fce6add0d6134bd68be775
beisen <luochongjie@beisen.com>
2018-10-09 16:24:38 +0800

    触发脚本

--- 动画/css.js
+++ 动画/css.js
@@ -1,10 +1,9 @@
-import spring,{toString} from "css-spring"
+import spring, { toString } from "css-spring";
 const keyframes = spring(
     { scale: 0 }, // from
     { scale: 1 }, // to
     // precision表示精度有2位
-    { damping: 14, stiffness: 170, precision: 2}
+    { damping: 14, stiffness: 170, precision: 2 }
 );
 const keyframeString = toString(keyframes);
 console.log(keyframeString);
-
